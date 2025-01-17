fc922bea456e7fc225a82159870dcb05b832d8f5
Alvaro <alvcomoli@alu.edu.gva.es>
2025-01-17 08:25:32 +0100

    Routes no OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/movil/header/header.component'; // Ajusta la ruta según tu estructura de carpetas
-import { DashboardComponent } from './components/movil//dashboard/dashboard.component'; 
+import { DashboardComponent } from './components/movil//dashboard/dashboard.component';
 import { FooterComponent } from './components/movil//footer/footer.component';
 import { TareasComponent } from "./components/movil/dashboard/tareas/tareas.component";
+import { RouterOutlet,RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, DashboardComponent, FooterComponent, TareasComponent], // Importa aquí el HeaderComponent
+  imports: [HeaderComponent, DashboardComponent, FooterComponent, TareasComponent,RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
