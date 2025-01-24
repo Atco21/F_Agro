dc01eac001e3ded9a8a433a33ad6466ee75f7b0a
Jieqiino <jieqinjiang031101@gmail.com>
2025-01-24 08:14:06 +0100

    Mostrar_Listado_Incidencias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IncidenciasComponent } from './components/movil/incidencias/incidencias.component';
+import { OrdenComponent } from './components/movil/orden/orden.component';
 // import { IncidenciaNoFinalizadaComponent } from './components/movil/incidencias/incidencia-no-finalizada/incidencia-no-finalizada.component';
 // import { IncidenciaFinalizadaComponent } from './components/movil/incidencias/incidencia-finalizada/incidencia-finalizada.component';
 
@@ -21,6 +22,10 @@ export const routes: Routes = [
         //     component: IncidenciaFinalizadaComponent,
         //   }
         // ]
+      },
+      {
+        path: 'orden',
+        component: OrdenComponent,
       }
     ]
   }
