63ec69d5b9b3ea3f06d0c9cd6fde992983048b66
Alvaro <alvcomoli@alu.edu.gva.es>
2025-01-25 00:03:12 +0100

    Resolviendo conflictos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/j_campo/header/header.component';
 import { FooterComponent } from './components/j_campo/footer/footer.component';
 import { ModalCrearTareaComponent } from './components/j_campo/modal-crear-tarea/modal-crear-tarea.component';
 import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule
 import { ModalCrearParcelaComponent } from './components/j_campo/modal-crear-parcela/modal-crear-parcela.component';
-=======
-import { FooterComponent } from './components/j_campo/footer/footer.component';
-import { RouterOutlet} from '@angular/router';
-import { HeaderComponent } from "./components/j_campo/header/header.component";
->>>>>>> 996d04ff7b174cb45c9f3dcab761e7f433b946af
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ModalCrearTareaComponent,ModalCrearParcelaComponent,FormsModule],
-=======
-  imports: [FooterComponent, RouterOutlet, HeaderComponent],
->>>>>>> 996d04ff7b174cb45c9f3dcab761e7f433b946af
+  imports: [FooterComponent, RouterOutlet, HeaderComponent, ModalCrearTareaComponent, FormsModule, ModalCrearParcelaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
