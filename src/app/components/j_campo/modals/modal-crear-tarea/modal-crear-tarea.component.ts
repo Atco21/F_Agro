40df4dc2f7e559c55e99f9fa7ad2b20766cbf2e8
Alvaro <alvcomoli@alu.edu.gva.es>
2025-01-28 21:28:28 +0100

    Buenas noches

--- src/app/components/j_campo/modals/modal-crear-tarea/modal-crear-tarea.component.ts
+++ src/app/components/j_campo/modals/modal-crear-tarea/modal-crear-tarea.component.ts
@@ -14,19 +14,7 @@ export class ModalCrearTareaComponent {
   fecha : string = 'Nombre de la Fecha';
 
   cerrarModal(): void {
-    const modalElement = document.getElementById('modalCrearTarea');
-
-    if (modalElement) {
-      modalElement.classList.remove('show');
-      modalElement.setAttribute('aria-hidden', 'true');
-      modalElement.removeAttribute('aria-modal');
-      modalElement.style.display = 'none';
-
-      const modalBackdrop = document.querySelector('.modal-backdrop');
-      if (modalBackdrop) {
-        modalBackdrop.remove();
-      }
-    }
+    // Aquí puedes agregar cualquier lógica adicional que necesites
+    console.log('Modal cerrado');
   }
- 
 }
