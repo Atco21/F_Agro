991ee5a3d408f7d1ce2b36a9170fa03bdab782f1
Alvaro <alvcomoli@alu.edu.gva.es>
2025-03-02 09:12:03 +0100

    arreglo ordenes en master y routes(alvaro)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';//Ng sirve paraque funcionen las importaciones de padre a hijo y viceversa
-import { Routes,RouterModule } from '@angular/router';
+import { Routes} from '@angular/router';
 import { DashboardComponent } from './components/j_campo/dashboard/dashboard.component';
 import { NotificacionesComponent } from './components/j_campo/notificaciones/notificaciones.component';
 import { CalendarioComponent } from './components/j_campo/calendario/calendario.component';
-import { IncidenciasComponent } from './components/j_campo/incidencias/incidencias.component';
 import { MenuComponent } from './components/j_campo/menu/menu.component';
 import { ModalCrearParcelaComponent } from './components/j_campo/modals/modal-crear-parcela/modal-crear-parcela.component';
 import { ModalCrearTratamientoComponent } from './components/j_campo/modals/modal-crear-tratamiento/modal-crear-tratamiento.component';
-import {ModalCrearTareaComponent} from './components/j_campo/modals/modal-crear-tarea/modal-crear-tarea.component';
+import { ModalCrearTareaComponent} from './components/j_campo/modals/modal-crear-tarea/modal-crear-tarea.component';
 import { CrearOrdenComponent} from './components/j_campo/modals/crear orden/crear-orden.component';
 import { TipoPersonalComponent } from './components/j_campo/incidencias/tipos/tipo-personal/tipo-personal.component';
 import { TipoMaquinaComponent } from './components/j_campo/incidencias/tipos/tipo-maquina/tipo-maquina.component';
@@ -16,9 +14,28 @@ import { OrdenEnCursoComponent } from './components/j_campo/orden/orden-curso/or
 import { OrdenPausadasComponent } from './components/j_campo/orden/orden-pausadas/orden-pausadas.component';
 import { OrdenPendientesComponent } from './components/j_campo/orden/orden-pendientes/orden-pendientes.component';
 import { OrdenTerminadasComponent } from './components/j_campo/orden/orden-terminadas/orden-terminadas.component';
+
+
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'incidencias', component: IncidenciasComponent },
+
+  {
+    path: "incidencias",
+    children: [
+      { path: "tipo-personal", component: TipoPersonalComponent }, // Tabla incidencias tipo personal
+      { path: "tipo-maquina", component: TipoMaquinaComponent },   // Incidencias tipo máquina
+      { path: "tipo-stock", component: TipoStockComponent }        // Incidencias tipo stock
+    ]
+  },
+  {
+    path: "orden",
+    children: [
+      {path: "orden-curso", component: OrdenEnCursoComponent},
+      {path: "orden-pausadas", component: OrdenPausadasComponent},
+      {path: "orden-pendientes", component: OrdenPendientesComponent}
+    ]
+  },
+
   { path: 'notificaciones', component: NotificacionesComponent },
   { path: 'calendario', component: CalendarioComponent },
   { path: 'menu', component: MenuComponent },
@@ -28,13 +45,8 @@ export const routes: Routes = [
   {path:'modal-crear-tarea', component: ModalCrearTareaComponent},
   {path:'dashboard', component: DashboardComponent},
   {path:'modal-crear-tratamiento', component:CrearOrdenComponent},
-  {path:"incidencias/tipo-personal", component:TipoPersonalComponent},//tabla incidencias tipo personal
-  {path:"incidencias/tipo-maquina", component:TipoMaquinaComponent},//incedncias tipo maquina
-  {path:"incidencias/tipo-stock",component:TipoStockComponent}, //incidencias tipo stock
   {path:"orden/orden-curso", component:OrdenEnCursoComponent},  //ruta de orden
-  {path:"orden/orden-pausadas",component:OrdenPausadasComponent},
-  {path:"orden/orden-pendientes",component:OrdenPendientesComponent},
-  {path:"orden/orden-terminadas",component:OrdenTerminadasComponent},
+
 
 
 ];
