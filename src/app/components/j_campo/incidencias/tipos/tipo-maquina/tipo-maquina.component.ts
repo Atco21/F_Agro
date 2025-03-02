79270064ca3af31ad827eb2bb6200d1dc2c63d1f
Alvaro <alvcomoli@alu.edu.gva.es>
2025-03-02 18:02:29 +0100

    servicios

--- src/app/components/j_campo/incidencias/tipos/tipo-maquina/tipo-maquina.component.ts
+++ src/app/components/j_campo/incidencias/tipos/tipo-maquina/tipo-maquina.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tipo-maquina',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tipo-maquina.component.html',
   styleUrl: './tipo-maquina.component.css'
 })
---
+++ src/app/components/j_campo/services/incidencias.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncidenciasService {
+
+  constructor(private http: HttpClient) {}
+
+
+  getIncidenciasPersonal() {
+    return this.http.get('http://localhost:80/api/incidenciasPersonal');
+  }
+
+  // Método para obtener incidencias de tipo máquina
+  getIncidenciasMaquina(){
+    return this.http.get('http://localhost:80/api/incidenciasMaquina');
+  }
+
+  // Método para obtener incidencias de tipo tratamiento
+  getIncidenciasTratamiento() {
+    return this.http.get('http://localhost:80/api/ordenesStrock');
+  }
+}
+
